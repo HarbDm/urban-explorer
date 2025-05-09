c98fd8a96ef532dcb2a16d77b4405e72f6e3bd8a
HarbDm <dmytroharbulinskyi@gmail.com>
2025-05-09 11:55:03 +0300

    project module structure updated

--- di/build.gradle.kts
+++ di/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
-    namespace = "com.harbdm.urbanexplorer.data"
+    namespace = "com.harbdm.urbanexplorer.di"
     compileSdk = 35
 
     defaultConfig {
@@ -36,33 +36,14 @@ android {
 
 dependencies {
     implementation(project(":domain"))
+    implementation(project(":data"))
     implementation(project(":core"))
 
-    // Kotlin & Coroutines
-    implementation(libs.kotlinx.coroutines.core)
-    implementation(libs.kotlinx.coroutines.android)
 
-    // Room (for local storage)
-    implementation(libs.androidx.room.runtime)
-    ksp(libs.androidx.room.compiler)
-    implementation(libs.androidx.room.ktx)
-
-    // Retrofit (for networking)
-    implementation(libs.retrofit)
-    implementation(libs.converter.gson)
-    implementation(libs.logging.interceptor)
-
-    // Hilt (for Dependency Injection)
+    // Hilt
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
 
-
-
-    // Core AndroidX
-    implementation(libs.androidx.core.ktx)
-
-    // Test dependencies
+    // Test Dependencies (mostly for verifying module setup, less for extensive logic)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit.v115)
-
 }
--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -1,23 +1,19 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-    id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
-    namespace = "com.harbdm.urbanexplorer"
+    namespace = "com.harbdm.urbanexplorer.presentation"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.harbdm.urbanexplorer"
         minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -36,31 +32,35 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures {
-        compose = true
-    }
 }
 
 dependencies {
-
-    implementation(project(":data"))
+    implementation(project(":domain"))
+    implementation(project(":core"))
 
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
 
-// Hilt (for ViewModel injection and other Android components)
-    // Hilt (for Dependency Injection)
+    // Compose
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.material3)
+    implementation(libs.ui.tooling.preview)
+    debugImplementation(libs.ui.tooling)
+
+    // Lifecycle & ViewModel for Compose
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.activity.compose)
+
+    // Navigation Compose
+    implementation(libs.androidx.navigation.compose)
+
+    // Hilt
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
-    // For Hilt navigation compose
     implementation(libs.androidx.hilt.navigation.compose)
+
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
